55a526dd9bc7e28fd09f684561af86ce323173da
Raz Carolino <russelle.carolino@infor.com>
2024-03-06 00:44:10 +0800

    feature/JSAI6 - Initial design of systems dashboard

--- App/Main.Designer.cs
+++ App/Main.Designer.cs
@@ -34,6 +34,7 @@
             this.btnCustomer = new System.Windows.Forms.Button();
             this.btnProduct = new System.Windows.Forms.Button();
             this.pnlModule = new System.Windows.Forms.Panel();
+            this.btnDashboard = new System.Windows.Forms.Button();
             this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -50,17 +51,18 @@
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel2.Location = new System.Drawing.Point(0, 0);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1019, 67);
+            this.panel2.Size = new System.Drawing.Size(1019, 34);
             this.panel2.TabIndex = 1;
             // 
             // panel3
             // 
+            this.panel3.Controls.Add(this.btnDashboard);
             this.panel3.Controls.Add(this.btnCustomer);
             this.panel3.Controls.Add(this.btnProduct);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel3.Location = new System.Drawing.Point(0, 67);
+            this.panel3.Location = new System.Drawing.Point(0, 34);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(272, 679);
+            this.panel3.Size = new System.Drawing.Size(272, 712);
             this.panel3.TabIndex = 2;
             // 
             // btnCustomer
@@ -68,7 +70,7 @@
             this.btnCustomer.Location = new System.Drawing.Point(17, 232);
             this.btnCustomer.Name = "btnCustomer";
             this.btnCustomer.Size = new System.Drawing.Size(238, 55);
-            this.btnCustomer.TabIndex = 1;
+            this.btnCustomer.TabIndex = 2;
             this.btnCustomer.Text = "&Customer";
             this.btnCustomer.UseVisualStyleBackColor = true;
             this.btnCustomer.Click += new System.EventHandler(this.btnCustomer_Click);
@@ -78,7 +80,7 @@
             this.btnProduct.Location = new System.Drawing.Point(17, 171);
             this.btnProduct.Name = "btnProduct";
             this.btnProduct.Size = new System.Drawing.Size(238, 55);
-            this.btnProduct.TabIndex = 0;
+            this.btnProduct.TabIndex = 1;
             this.btnProduct.Text = "&Product";
             this.btnProduct.UseVisualStyleBackColor = true;
             this.btnProduct.Click += new System.EventHandler(this.btnProduct_Click);
@@ -87,10 +89,20 @@
             // 
             this.pnlModule.BackColor = System.Drawing.Color.White;
             this.pnlModule.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pnlModule.Location = new System.Drawing.Point(272, 67);
+            this.pnlModule.Location = new System.Drawing.Point(272, 34);
             this.pnlModule.Name = "pnlModule";
-            this.pnlModule.Size = new System.Drawing.Size(747, 679);
+            this.pnlModule.Size = new System.Drawing.Size(747, 712);
             this.pnlModule.TabIndex = 3;
+            // 
+            // btnDashboard
+            // 
+            this.btnDashboard.Location = new System.Drawing.Point(17, 110);
+            this.btnDashboard.Name = "btnDashboard";
+            this.btnDashboard.Size = new System.Drawing.Size(238, 55);
+            this.btnDashboard.TabIndex = 0;
+            this.btnDashboard.Text = "&Dashboard";
+            this.btnDashboard.UseVisualStyleBackColor = true;
+            this.btnDashboard.Click += new System.EventHandler(this.btnDashboard_Click);
             // 
             // Main
             // 
@@ -105,6 +117,7 @@
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Main";
             this.Text = "Main";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.panel3.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -118,5 +131,6 @@
         private System.Windows.Forms.Panel pnlModule;
         private System.Windows.Forms.Button btnProduct;
         private System.Windows.Forms.Button btnCustomer;
+        private System.Windows.Forms.Button btnDashboard;
     }
 }
