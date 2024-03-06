f8e0d5176b6bb38e994f5a7968b2ce2a5bd48a3c
RgieCrl <reggie.carolino@gmail.com>
2024-03-06 19:19:40 +0800

    feature/JSAI4 - Design Customer Interface

--- App/Customer/Customer.cs
+++ App/Customer/Customer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using App.Customer;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,13 +9,36 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace App
+namespace App.Customer
 {
     public partial class Customer : Form
     {
         public Customer()
         {
             InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            using (CustomerModal cmodal = new CustomerModal())
+            {
+                cmodal.ShowDialog();
+            }
+            this.Show();
+        }
+
+        private void Customer_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            using (CustomerModal cmodal = new CustomerModal())
+            {
+                cmodal.ShowDialog();
+            }
+            this.Show();
         }
     }
 }
