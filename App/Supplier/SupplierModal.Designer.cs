0b67ff663728a91161d4846d15dd804b90fa3303
RgieCrl <reggie.carolino@gmail.com>
2024-03-17 00:58:06 +0800

    changes in supplier modal

--- App/Supplier/SupplierModal.Designer.cs
+++ App/Supplier/SupplierModal.Designer.cs
@@ -28,19 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(100, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(316, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "New Supplier Data Entry";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(1, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(546, 41);
+            this.panel1.TabIndex = 1;
             // 
             // SupplierModal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(470, 487);
+            this.ClientSize = new System.Drawing.Size(548, 304);
+            this.Controls.Add(this.panel1);
             this.Name = "SupplierModal";
             this.Text = "SupplierModal";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
