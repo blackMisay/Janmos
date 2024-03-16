68e69fe22fdf9578f22089a6511ab4d4f9ccd6a7
RgieCrl <reggie.carolino@gmail.com>
2024-03-17 00:01:09 +0800

    feature/JSAI18 - Design of Supplier Interface

---
+++ App/Supplier/Supplier.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App.Supplier
+{
+    public partial class Supplier : Form
+    {
+        public Supplier()
+        {
+            InitializeComponent();
+        }
+
+        private void Supplier_Load(object sender, EventArgs e)
+        {
+            this.LoadSupplierData();
+        }
+
+        private void LoadSupplierData()
+        {
+            
+        }
+    }
+}
